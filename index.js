a72208dfd742e33641954d3308ac8017bb373437
d00d1e <lee.julianay@gmail.com>
2021-02-02 18:09:20 -0800

    REST API endpoints

--- index.js
+++ index.js
@@ -1,15 +1,68 @@
 const express = require('express');
 const morgan = require('morgan'); //HTTP request logger middleware function
+const bodyParser = require('body-parser');
+const uuid = require('uuid');
 const app = express();
 
 
 app.use(express.static('public')); //routes all requests for static files to 'public' folder
 app.use(morgan('common')); //request log using Morgans 'common' format
+app.use(bodyParser.json());
 
 
+//--- API Endpoints ---//
+
+// GET- default response when request hits root folder
 app.get('/', (req, res) => {
-  res.send('Homepage');
+  var responseText = "Welcome to iFlix!!";
+  res.send(responseText);
 });
+
+// GET- return a list of all movies
+app.get('/movies', (req, res) => {
+  res.send("List of all movies");
+});
+
+// GET- return data for a single movie by title
+app.get('/movies/:title', (req, res) => {
+  res.send("get single movie title")
+})
+
+// GET- return data about a specific director
+app.get('/movies/director/:Director', (req, res) => {
+  res.send("Director info");
+});
+
+// GET- return data about a specific genre
+app.get('/movies/genre/:Genre', (req, res) => {
+  res.send("Genre info");
+});
+
+// POST- allow new user to register
+app.post('/users', (req, res) => {
+  res.send('user registered')
+});
+
+// PATCH-  allow user to update user info
+app.patch('/users/:userId', (req, res) => {
+  res.send("user info updated");
+})
+
+// POST- allow user to add a movie to list of favorites
+app.post('/users/:username/favorites/:movieid', (req, res) => {
+  res.send("movie added to favorites");
+});
+
+// DELETE- allow user to remove a movie from list of favorites
+app.delete('/users/:username/favorites/:movieid', (req, res) =>{
+  res.send("movie removed from favorites")
+});
+
+// DELETE- allow existing users to deregister
+app.delete('/users/:username', (req, res) => {
+  res.send('user account deleted');
+});
+
 
 // error handling middleware
 app.use((err, req, res, next) => {
